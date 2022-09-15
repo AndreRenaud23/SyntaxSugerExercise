8bb53d7a29e31c9b44e9d9f6bf41ef68fc9197a6
AndreRenaud23 <andrer.mailbox@gmail.com>
2022-09-15 11:26:27 -0500

    First commit

---
+++ SyntaxSugerExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿// Orignal code version 1.00.0
+
+//int answer = 4;
+//string response;
+//if (answer < 9)
+//{
+//    response = $"{answer} is less than nine";
+//}
+//else
+//{
+//    response = $"{answer} is greater than or equal to nine";
+//}
+
+// New version 1.00.1
+
+int answer = 4;
+string response = answer < 9 ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+Console.WriteLine(response);
